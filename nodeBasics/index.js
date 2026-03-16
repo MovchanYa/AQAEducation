2200fdd782f0034b4ee2b6fe9d7ebff1fe1dee0a
MovchanYa <0chanbanan0@gmail.com>
2026-03-16 14:14:06 +0200

    chalk implementation

---
+++ nodeBasics/index.js
@@ -0,0 +1,10 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Yeah'));
+console.log(chalk.bgRed.bold('uh-huh'));
+console.log(chalk.underline.blue('you know what it is'));
+console.log(chalk.bgWhite.black('Black') + chalk.blue(' and, ') + chalk.bgYellow.bold('Yellow'));
+console.log(chalk.bgWhite.black('Black') + chalk.blue(' and, ') + chalk.bgYellow.bold('Yellow'));
+console.log(chalk.underline.blue('you know what it is'));
+console.log(chalk.bgWhite.black('Black') + chalk.blue(' and, ') + chalk.bgYellow.bold('Yellow'));
+console.log(chalk.bgWhite.black('Black') + chalk.blue(' and, ') + chalk.bgYellow.bold('Yellow'));
