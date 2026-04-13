8d36f90efae6ab51e87c8d6796d31233a78db4c9
MovchanYa <0chanbanan0@gmail.com>
2026-04-13 18:41:46 +0300

    fix task5

--- lesson9/task5.js
+++ lesson9/task5.js
@@ -1,10 +1,16 @@
 const users = [
     { name: "Alice", email: "alice@gmail.com", age: 30 },
-    { name: "Bob", email: "bob@gmail.com", age: 25 },
-    { name: "Rom", email: "rom@gmail.com", age: 35 },
-    { name: "David", email: "david@gmail.com", age: 40 }
+    { name: "Bob", age: 25 },
+    { email: "rom@gmail.com" },
+    { age: 40 },
+    { }
 ];
 
-for (const user of users) {
-    console.log(`Name: ${user.name}, Email: ${user.email}, Age: ${user.age}`);
+for (const { name, email, age } of users) {
+
+    const eName = name || false;
+    const eEmail = email ?? false;
+    const eAge = age || false;
+
+    console.log(`Name: ${eName}, Email: ${eEmail}, Age: ${eAge}`);
 }
